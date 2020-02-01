74e50b65a498ed41e5ca85bfbe9beb9c1fd2c1fc
kraft-owaka <owakakraft@gmail.com>
2020-02-01 21:08:07 +0300

    added a route function that wil add my stories

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,17 @@
-from flask import render_template
-from app import app
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_source,get_articles
+from ..model import Source
 
 # Views
 @main.route('/') #is a route decorator
-def index(): # function
+def index(): # view function
 
     '''
     View root page function that returns the index page and its data
-    '''
+    ''' 
+    general_news = get_source('entertainment')
+    print('*-*-*Stories*-*-*')
+    print(general_news)
+    business_news=get_source('business')
     return render_template('index.html')
