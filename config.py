a72a128b0a314c3936dc3dda573cdab8f16692d1
kraft-owaka <owakakraft@gmail.com>
2020-02-01 22:44:20 +0300

    To make our application use the new configurations

--- config.py
+++ config.py
@@ -1,1 +1,32 @@
 import os
+
+class Config:
+    '''
+    configration of the parent class
+    '''
+    SOURCE_BASE_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLE_BASE_URL='https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    API_KEY = os.environ.get('API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEBUG = True
+
+    config_option = {
+        'development':DevConfig,
+        'production':ProdConfig
+    }
