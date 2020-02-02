783af2184daa6d7d12b35e4fe5db4859153f09a0
kraft-owaka <owakakraft@gmail.com>
2020-02-02 06:41:38 +0300

    made changes into the request

--- app/model.py
+++ app/model.py
@@ -1,1 +1,33 @@
-from 
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,id,name,description,url,category,language,country):
+        self.id=id
+        self.name=name
+        self.description = description
+        self.url = url
+        self.category=category
+        self.language=language
+        self.country=country
+
+class Article:
+
+    def __init__(self,id,author,title,description,url,image,date):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.image= image
+        self.date = date
+
+## class Sources:
+#     '''
+#     Sources class defining Source Objects
+#     '''
+#     def __init__(self,name,description,source_url):
+#         self.name = name
+#         self.description = description
+#         self.source_url = source_url
--- app/request.py
+++ app/request.py
@@ -1,7 +1,7 @@
 import urllib.request,json
 # this will aid in creating a link/connectio to our API URLthen sends the request, the jason module will format the JSON response 
 from .model import Source, Article
-
+t behaviour of the Movie class
 api_key=None
 base_url=None
 articles_url=None 
